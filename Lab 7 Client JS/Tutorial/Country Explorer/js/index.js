e8b770f4ce8265e28a0875fce12a30c36fa4619e
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-03-20 15:21:41 +0300

    changeCountryFacts updated

--- Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
+++ Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
@@ -4,7 +4,30 @@ const countryURL = 'https://restcountries.com/v3.1/name/'
 // 2. The reference to the dom elements
 const regionDL = document.querySelector("#region")
 const countryDD = document.querySelector("#country")
+const factsArea = document.querySelector("#facts-area")
 
-function handleRegionChange() {
-    console.log(`${regionURL}${regionDL.value}`)
+async function handleRegionChange() {
+    const url = `${regionURL}${regionDL.value}`
+    const data = await fetch(url)
+    const countries = await data.json()
+
+    countryDD.innerHTML = countries
+        .map(country => `<option value="${country.name.common}">${country.name.common}</option>`)
+        .join(' ')
+}
+
+async function handleCountryChange() {
+
+    const url = `${countryURL}${countryDD.value}`
+    const data = await fetch(url)
+    const country = await data.json()
+    const htmlContent = changeCountryToFacts(country[0])
+    factsArea.innerHTML = htmlContent
+}
+
+function changeCountryToFacts(country) {
+    return `
+        <h2> Facts About ${country.name.common}</h2>
+        <img src="" alt="">
+    `
 }
