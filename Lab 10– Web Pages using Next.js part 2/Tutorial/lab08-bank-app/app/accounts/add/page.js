ee1c3e0a9b34e1a38254e0af07db70cfe89cd969
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-04-17 14:27:34 +0300

    updated

--- Lab 10– Web Pages using Next.js part 2/Tutorial/lab08-bank-app/app/accounts/add/page.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Add() {
-    return (
-        <div>I am the add page</div>
-    )
-}
--- Lab 10– Web Pages using Next.js part 2/Tutorial/lab08-bank-app/app/accounts/trans/page.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Trans() {
-    return (
-        <div>I am the transaction page</div>
-    )
-}
