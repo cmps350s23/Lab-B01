ffe276afa4128140287b249acc421b82d5770351
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-04-17 15:54:58 +0300

    updated

--- Lab 10– Web Pages using Next.js part 2/Tutorial/lab08-bank-app/app/accounts/add/page.js
+++ Lab 10– Web Pages using Next.js part 2/Tutorial/lab08-bank-app/app/accounts/add/page.js
@@ -1,7 +1,43 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import styles from '../../page.module.css'
+import { useRouter } from 'next/navigation'
 
 export default function Add() {
+    const [account, setAccount] = useState({})
+    const router = useRouter()
+
+    function handleChange(e) {
+        const newAccount = { ...account }
+        newAccount[e.target.name] = e.target.value
+        setAccount(newAccount)
+    }
+    async function handleSubmit(e) {
+        e.preventDefault()
+        const response = await fetch('/api/accounts', {
+            method: 'POST',
+            body: JSON.stringify(account),
+            headers: { 'Content-Type': 'application/json' }
+        })
+        console.log(await response.json());
+        router.push('/', { shallow: true })
+    }
     return (
-        <div>Add</div>
+        <>
+            <h3 className={styles.title}>Add Account</h3>
+            <form id="account-form" className={styles.form} onSubmit={handleSubmit}>
+                <label htmlFor="acctType" className={styles.label}>Account Type</label>
+                <select name="acctType" id="acctType" required onChange={handleChange}>
+                    <option></option>
+                    <option value="Saving">Saving</option>
+                    <option value="Current">Current</option>
+                </select>
+
+                <label htmlFor="balance" className={styles.label}>Balance</label>
+                <input type="number" name="balance" id="balance" required onChange={handleChange} />
+
+                <button type="Submit">Submit</button>
+            </form>
+        </>
     )
 }
