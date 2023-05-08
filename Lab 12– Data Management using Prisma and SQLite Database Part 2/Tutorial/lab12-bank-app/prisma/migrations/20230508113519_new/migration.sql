19ad9d208bde11aa657ee52b8e2b0f9685f5e938
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-05-08 15:21:25 +0300

    updated code of delete, get owners and avg

---
+++ Lab 12– Data Management using Prisma and SQLite Database Part 2/Tutorial/lab12-bank-app/prisma/migrations/20230508113519_new/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `addedNewField` on the `Transaction` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Transaction" (
+    "transId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "transType" TEXT NOT NULL,
+    "amount" REAL NOT NULL,
+    "date" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "newfield" TEXT NOT NULL DEFAULT 'default value',
+    "accountNo" TEXT NOT NULL,
+    CONSTRAINT "Transaction_accountNo_fkey" FOREIGN KEY ("accountNo") REFERENCES "Account" ("accountNo") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Transaction" ("accountNo", "amount", "date", "transId", "transType") SELECT "accountNo", "amount", "date", "transId", "transType" FROM "Transaction";
+DROP TABLE "Transaction";
+ALTER TABLE "new_Transaction" RENAME TO "Transaction";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
